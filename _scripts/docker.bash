1bb4a09ffb70711946f37b24746af1cb7845d4b4
Radon Rosborough <radon.neon@gmail.com>
2021-07-13 17:14:41 -0700

    Fix up Docker usage for CI

--- _scripts/docker.bash
+++ _scripts/docker.bash
@@ -22,10 +22,12 @@ if [[ -z "${NO_BUILD:-}" ]]; then
     docker build . -t intuitive-explanations --build-arg "UID=$UID"
 fi
 
-# Putting the deploy key in the command line is considered bad
-# practice. However we must do it to work around a Docker bug, see
-# <https://github.com/moby/moby/issues/40221>.
-docker run -it --rm -v "$PWD:/home/docker/src" \
-       -e "PORT=${PORT}" -e "HOST=0.0.0.0" -e "NETLIFY_KEY=${NETLIFY_KEY}" \
+it=
+if [[ -t 1 ]]; then
+    it="-it"
+fi
+
+docker run ${it} --rm -v "$PWD:/home/docker/src" \
+       -e "PORT=${PORT}" -e "HOST=0.0.0.0" -e NETLIFY_KEY \
        -p "${PORT}:${PORT}" \
        intuitive-explanations "${args[@]}"
