9779c1749d2519edd17882e2efd0bec7b7d48aa8
Radon Rosborough <radon@intuitiveexplanations.com>
2026-02-09 18:36:30 -0800

    Change the sudo to work more robustly

--- tools/docker-pid1.bash
+++ tools/docker-pid1.bash
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 cat <<"EOF" > /etc/sudoers.d/intuitive-explanations
-%sudo ALL=(ALL:ALL) NOPASSWD: ALL
+docker ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
 
 groupadd -g "$(stat -c %g "$PWD")" -o -p '!' -r docker
-useradd -u "$(stat -c %u "$PWD")" -g "$(stat -c %g "$PWD")" -o -p '!' -m -N -l -s /usr/bin/bash -G sudo docker
+useradd -u "$(stat -c %u "$PWD")" -g "$(stat -c %g "$PWD")" -o -p '!' -m -N -l -s /usr/bin/bash docker
 
 runuser -u docker touch /home/docker/.sudo_as_admin_successful
 runuser -u docker npm install
